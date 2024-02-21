103daab9021352ac1fca4b9a0358d93f72c5f200
Hadi Abedah <artificialache@gmail.com>
2024-02-21 13:57:51 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -63,6 +63,8 @@ class Dev(Configuration):
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
         'rest_framework',
+        'rest_framework.authtoken'
+        'django_filters'
     ]
 
     MIDDLEWARE = [
@@ -230,6 +232,13 @@ class Dev(Configuration):
             "user_sustained": "5000/day",
             "user_burst": "100/minute",
         },
+
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
+    "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+            "rest_framework.filters.OrderingFilter"
+        ],
     }
 class Prod(Dev): 
     DEBUG = False
--- blog/test_post_api.py
+++ blog/test_post_api.py
@@ -1,43 +1,4 @@
-from datetime import datetime
 
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-from django.utils import timezone
-from pytz import UTC
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APIClient
-
-from blog.models import Post
-
-class PostApiTestCase(TestCase):
-    def setUp(self):
-        self.u1 = get_user_model().objects.create_user( 
-            email="test@example.com", password="password"
-        )
-        self.u2 = get_user_model().objects.create_user(
-            email="test2@example.com", password="password2"
-        )
-
-        self.post1 = Post.objects.create(
-            author=self.u1,
-            published_at=timezone.now(),
-            title="Post 1 Title",
-            slug="post-1-slug",
-            summary="Post 1 Summary",
-            content="Post 1 Content",
-        )
-        self.post2 = Post.objects.create(
-            author=self.u2,
-            published_at=timezone.now(),
-            title="Post 2 Title",
-            slug="post-2-slug",
-            summary="Post 2 Summary",
-            content="Post 2 Content",
-        )
-
-        self.client = APIClient() 
-        token = Token.objects.create(user=self.u1)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
       
 from datetime import datetime
 
@@ -82,7 +43,7 @@ class PostApiTestCase(TestCase):
 
     def test_post_list(self):
         resp = self.client.get("/api/v1/posts/")
-        data = resp.json()
+        data = resp.json()["results"]
         self.assertEqual(len(data), 2)
 
         for post_dict in data:
